db3ea1e65867fe1f31c12ae6286fcf4f22197cc6
Dean Chouinard <deanchouinard@gmail.com>
2016-07-26 22:40:52 -0400

    first commit

---
+++ lib/scraper.ex
@@ -0,0 +1,37 @@
+defmodule Scraper do
+  use Hound.Helpers
+
+  def start do
+    IO.puts "starting"
+    Hound.start_session
+    navigate_to "https://example.com/login"
+    find_element(:id, "user_login") |> fill_field('deanc')
+    el = find_element(:id, "user_pass")
+    fill_field(el, 'passwordblah')
+    submit_element(el)
+  end
+
+  def download(src, output_filename) do
+    IO.puts "Downloading #{src} -> #{output_filename}"
+    #body = HTTPoison.get!(src).body
+    body = HTTPoison.get!(src).body
+    File.write!(output_filename, body)
+    IO.puts "Done downloading #{src} -> #{output_filename}"
+  end
+
+  def get_video(url, output_filename) do
+    navigate_to url
+    page_source() |> Floki.find("source") |> Floki.attribute("src")
+      |> IO.inspect |> download(output_filename)
+  end
+  
+  def get_video_sp(url, output_filename) do
+    navigate_to url
+    #video_src = page_source() |> Floki.find("source") |> Floki.attribute("src")
+    video_src = page_source()
+    spawn(Scraper, :download, [video_src, output_filename])
+
+    #  |> IO.inspect |> download(output_filename)
+  end
+
+end
